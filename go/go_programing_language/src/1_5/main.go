e315eb0fb11e994136b3de62f73236fdb5f3162c
jsmack <jsmackerel@gmail.com>
2018-09-14 03:52:27 +0000

    fix

--- go/go_programing_language/src/1_5/main.go
+++ go/go_programing_language/src/1_5/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+    "io/ioutil"
 )
 
 func main() {
@@ -12,6 +13,8 @@ func main() {
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "https://") {
 			url = "https://" + url
+         } else if !strings.HasPrefix(url, "http://") {
+             url = "http://" + url
 		}
 
 		resp, err := http.Get(url)
@@ -24,12 +27,12 @@ func main() {
 		// 	fmt.Fprintf(os.Stderr, "1_5: %v\n", err)
 		// }
 		fmt.Println(resp.Status)
-		//b, err := ioutil.ReadAll(resp.Body)
-		// resp.Body.Close()
-		// if err != nil {
-		// 	fmt.Fprintf(os.Stderr, "1_5: reading %s: %v\n", url, err)
-		// 	os.Exit(1)
-		// }
-		// fmt.Printf("%s", bb)
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "1_5: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
 	}
 }
