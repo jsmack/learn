247058d7cc31d230057860f65ddfe4f7624ba99b
jsmack <32086618+jsmack@users.noreply.github.com>
2020-01-15 23:17:43 +0900

    Rust (#25)
    * add 5.3 method written
    
    * fix: chage directory

---
+++ language/rust/bkup/chapter/06/match_allow/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents( coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 15,
+    }
+}
+
