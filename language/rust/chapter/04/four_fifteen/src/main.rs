0786e92ce590964599a3ba0c7dc078e7edfb4909
jsmack <jsmackerel@gmail.com>
2020-01-23 23:11:20 +0900

    add 5.16-2

--- language/rust/chapter/04/four_fifteen/src/main.rs
+++ language/rust/chapter/04/four_fifteen/src/main.rs
@@ -32,11 +32,32 @@ fn main() {
     match origin {
         Point { y, .. } => println!("{}", y),
     }
-    
+
+    let tuple: (u32, String) = (5, String::from("hoge"));
+    // let (x, _s) = tuple;
+    // println!("{}", x);
+    let (y, _) = tuple;
+    println!("{}", y);
+    println!("{:#?}", tuple);
+
+    let x2 = 1;
+
+    match x2 {
+        1 ... 5 => println!("one through five"),
+        _ => println!("anything"),
+    }
+
+    let name = "Steve".to_string();
+    let xx: Option<Person> = Some(Person { name: Some(name) });
 }
 
 
 struct Point {
     x: i32,
     y: i32,
+}
+
+#[derive(Debug)]
+struct Person {
+    name: Option<String>,
 }
