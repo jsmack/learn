d6e4a62b592291d943002c0b48923753dc818787
jsmack <32086618+jsmack@users.noreply.github.com>
2020-01-26 21:29:06 +0900

    Rust (#34)
    * add 5.16-2
    
    * add 4.16
    
    * fix: rename
    
    * add 4.17
    
    * add 4.18

---
+++ language/rust/chapter/04/x_04_19/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+// impl Circle {
+//     fn area(&self) -> f64 {
+//         std::f64::consts::PI * (self.radius * self.radius)
+//     }
+// }
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
