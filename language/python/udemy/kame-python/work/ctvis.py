ede5c222e4b15dc49c45ab87844a7b0b9c7d8367
jsmack <jsmackerel@gmail.com>
2021-07-06 21:51:00 +0900

    20210706

--- language/python/udemy/kame-python/work/ctvis.py
+++ language/python/udemy/kame-python/work/ctvis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from glob import glob
+import numpy as np
+import nibabel as nib
 
 # _をつけるとprivate functiontとして定義するが呼ぶことはできる
 # from ctvis import * だと_から始まるfunctionは呼ばれない
@@ -10,6 +12,18 @@ def _get_df4(base_path='public_covid_data', folder='rp_im'):
     return data_dict
 
 def get_df_all3(base_path='public_covid_data'):
-    rp_im_df = get_df4(base_path, folder='rp_im')
-    rp_msk_df = get_df4(base_path, folder='rp_msk')
+    rp_im_df = _get_df4(base_path, folder='rp_im')
+    rp_msk_df = _get_df4(base_path, folder='rp_msk')
     return rp_im_df.merge(rp_msk_df, on='FileName', suffixes=('Image', 'Mask'))
+
+
+def load_nifti(path):
+    #　nibでロード
+    nifti = nib.load(path)
+    
+    # 画像配列にアクセスするため
+    data = nifti.get_fdata()
+    
+    # 配列の回転
+    data_rolled =  np.rollaxis(data, axis=1, start=0)
+    return data_rolled
