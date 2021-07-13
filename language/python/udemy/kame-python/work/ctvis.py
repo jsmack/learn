22c1a067c577831a7e17855ee0255d76ff0515a0
jsmack <32086618+jsmack@users.noreply.github.com>
2021-07-13 21:22:51 +0900

    20210713 (#105)

--- language/python/udemy/kame-python/work/ctvis.py
+++ language/python/udemy/kame-python/work/ctvis.py
@@ -49,3 +49,27 @@ def label_color(mask_volume,
     mask_color[np.equal(mask_volume, 3 )] = effusion_color
     
     return mask_color
+
+def hu_to_gray(volume):
+    maxhu = np.max(volume)
+    # 最小値を取得
+    minhu = np.min(volume)
+    # 数値を0-1に変換 0になったらinfinityになるのでmax関数で0なら10-3乗(０．００1)にする
+    volume_rerange = (volume - minhu ) / max((maxhu - minhu), 1e-3)
+    # 値 * 255にする
+    volume_rerange = volume_rerange * 255
+
+    # 3次元(RGB)するため、stackで追加する。axisの-1で最後に追加
+    volume_rerange = np.stack([volume_rerange, volume_rerange, volume_rerange], axis=-1)
+    
+    return volume_rerange.astype(np.uint8)
+    
+
+def overlay(gray_volume, mask_volume, mask_color, alpha=0.3):
+    mask_filter = np.greater(mask_volume, 0)
+    mask_filter = np.stack([mask_filter, mask_filter, mask_filter], axis=-1)
+    
+    overlayed = np.where(mask_filter > 0,
+                         ((1 - alpha) * gray_volume + alpha * mask_color).astype(np.uint8), gray_volume)
+    
+    return overlayed
