e8865db860acfd7b59bc3e9ba368bc620c6fe47b
jsmack <jsmackerel@gmail.com>
2021-07-23 09:47:29 +0900

    20210723

--- language/python/udemy/kame-python/work/ctvis.py
+++ language/python/udemy/kame-python/work/ctvis.py
@@ -2,6 +2,7 @@ import pandas as pd
 from glob import glob
 import numpy as np
 import nibabel as nib
+import matplotlib.pyplot as plt
 
 # _をつけるとprivate functiontとして定義するが呼ぶことはできる
 # from ctvis import * だと_から始まるfunctionは呼ばれない
@@ -73,3 +74,67 @@ def overlay(gray_volume, mask_volume, mask_color, alpha=0.3):
                          ((1 - alpha) * gray_volume + alpha * mask_color).astype(np.uint8), gray_volume)
     
     return overlayed
+
+
+def vis_overlay(overlayed, original_vlume, mask_volume, cols=5, display_num=25, figsize= (15, 15)):
+    
+
+    # 列数 全体の表示数から行数であまりなしで割って+1
+    rows= (display_num - 1) // cols + 1
+
+    # (630, 630, 66, 3) の後ろから２個目が枚数
+    total_num = overlayed.shape[-2]
+
+    # 表示する画像の感覚を　全体の枚数から表示したい枚数を割るが、0.xxとかだと0になるのでその場合1を設定する
+    interval = total_num / display_num
+    if interval < 1:
+        interval = 1
+
+    # 画像のサイズ
+    fig, ax = plt.subplots(rows, cols, figsize=figsize)
+
+    for i in range(display_num):
+        row_i = i // cols
+        col_i = i % cols
+        # 表示する画像を設定
+        idx = int((i * interval))
+
+        #　インデックス数がが総数を上回った場合に抜ける
+        if idx >= total_num:
+                break
+        
+        stats = get_hu_stats(original_vlume[:, :, idx], mask_volume[:, :, idx])
+        title = f'slice #:{idx}'
+        #title += f'ggo mean: {stats['ggo_mean']}±{stats['ggo_std']}'
+        title += '\nggo mean: {:.0f}±{:.0f}'.format(stats['ggo_mean'], stats['ggo_std'])
+        #title += f'consolidation mean: {stats['consolidation_mean']}±{stats['consolidation_std']}'
+        title += '\nconsolidation mean: {:.0f}±{:.0f}'.format(stats['consolidation_mean'], stats['consolidation_std'])
+        #title += f'effusion mean: {stats['effusion_mean']}±{stats['effusion_std']}'
+        title += '\neffusion mean: {:.0f}±{:.0f}'.format(stats['effusion_mean'], stats['effusion_std'])
+        ax[row_i, col_i].imshow(overlayed[:, :, idx])
+        ax[row_i, col_i].set_title(title)
+        ax[row_i, col_i].axis('off')
+        
+    # 文字と画像が重なって見にくいので修正
+    fig.tight_layout()
+        
+        
+        
+def get_hu_stats(volume,
+                 mask_volume,
+                 label_dict = {1: 'ggo', 2: 'consolidation', 3: 'effusion'}):
+    
+    
+    result = {}
+    for label in label_dict.keys():
+        # labelを設定
+        prefix = label_dict[label]
+
+        roi_hu = volume[np.equal(mask_volume, label)]
+
+        result[prefix + '_mean'] = np.mean(roi_hu)
+        result[prefix + '_std']  = np.std(roi_hu)
+
+        
+    return result
+        
