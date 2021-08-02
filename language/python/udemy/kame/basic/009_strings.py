99e416c3281867f55e84b5530c28c58e96f61ad4
jsmack <jsmackerel@gmail.com>
2021-08-02 21:47:21 +0900

    20210802

---
+++ language/python/udemy/kame/basic/009_strings.py
@@ -0,0 +1,13 @@
+print(f"hello world!!")
+print(f'hello world!!')
+print(1)
+print("""
+Hello
+world
+How are you doing?
+""")
+print("Hello\nworld!")
+
+print("Hell\\nworld!")
+
+print('Hello' + ' ' + 'world' + '!!')
