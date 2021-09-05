aec150ea9c7aba2d94309b03a9930f8f58c082cd
jsmack <jsmackerel@gmail.com>
2021-09-05 21:48:59 +0900

    20210905

---
+++ language/python/udemy/kame-python/module/059_2_import.py
@@ -0,0 +1,12 @@
+# from module import func or variable
+from mymodule import myfunc, myvariable, anogherfunc
+# from mymodule import *
+
+myfunc()
+# This is my function
+
+print(myvariable)
+# This is global variable
+
+print(anogherfunc)
+# This is anogher function!!
---
+++ language/python/udemy/kame-python/module/060_sysmodule.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("../function/")
+import docstrings
+
